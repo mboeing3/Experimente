4f81c368f7e4cc35119f0d8797acf906c13ad46a
Marcel <devnull@localhost>
2014-02-21 10:57:43 +0100

    - Code bereinigt, kommentiert und vereinfacht.

--- src/de/mindlessbloom/suffixtree/BaumBauer.java
+++ src/de/mindlessbloom/suffixtree/BaumBauer.java
@@ -9,73 +9,76 @@ public class BaumBauer {
 	
     /**
      * Erzeugt einen Suffixbaum im uebergebenen Graphen ab dem uebergebenen
-     * Knoten anhand der uebergebenen Token.
-     * @param token Tokenarray
-     * @param rootnode Startknoten
-     * @param graph Graph
+     * Knoten anhand der uebergebenen Token. Inkrementiert die Zaehlvariable
+     * eines jeden Knotens um eins fuer jede "Beruehrung".
+     * @param token String-Array mit Token (Woerter, Buchstaben, Symbole, ... egal was)
+     * @param rootnode Startknoten Wurzelknoten des zu konstruierenden Baumes
+     * @param graph Graph Darf null sein
      * @param umgekehrt Zeigt an, ob der Baum umgekehrt erstellt werden soll (quasi als "Praefixbaum")
      * @return Die Anzahl der neu erstellten Knoten
      */
-    public int baumBuilder(String[] token, Knoten rootnode, Graph<Knoten, Kante> graph,
-	    boolean umgekehrt) {
+    public int baumBuilder(String[] token, Knoten rootnode, Graph<Knoten, Kante> graph, boolean umgekehrt) {
 
-	int knotenerstellt = 0;
+    	// Variable zum Mitzaehlen der erstellten Knoten
+		int knotenerstellt = 0;
 
-	if (token == null || token.length == 0) {
-	    return knotenerstellt;
-	}
+		if (token == null || token.length == 0) {
+			return knotenerstellt;
+		}
 
-	int vergleichsTokenIndex = 0;
-	if (umgekehrt)
-	    vergleichsTokenIndex = token.length - 1;
+		int vergleichsTokenIndex = 0;
+		if (umgekehrt) {
+			vergleichsTokenIndex = token.length - 1;
+		}
 
-	// passenden Kindknoten suchen
+		// Variable fuer Kindknoten definieren
+		Knoten kindKnoten;
 
-	// Ggf. neuen Knoten erstellen
-	if (!rootnode.getKinder().containsKey(token[vergleichsTokenIndex])) {
-	    // passender Knoten NICHT vorhanden - neuen erstellen und rekursiv
-	    // aufrufen
-	    Knoten neuerKnoten = new Knoten();
-	    knotenerstellt++;
-	    // Den Namen der Kante in der Node speichern .. fuer geordnete
-	    // Ausgabe spaeter
-	    neuerKnoten.setName(token[vergleichsTokenIndex].intern());
-	    Kante neueKante = new Kante(
-		    token[vergleichsTokenIndex].intern());
-	    rootnode.getKinder().put(token[vergleichsTokenIndex], neuerKnoten);
-	    if (graph != null) {
-		graph.addEdge(neueKante, rootnode, neuerKnoten,
-			EdgeType.DIRECTED);
-	    }
+		// Ggf. neuen Knoten erstellen
+		if (!rootnode.getKinder().containsKey(token[vergleichsTokenIndex])) {
+			// passender Knoten NICHT vorhanden - neuen erstellen
+			kindKnoten = new Knoten();
+			
+			// Zaehler fuer erstellte Knoten inkrementieren
+			knotenerstellt++;
+			
+			// Den Namen der Kante in der Node speichern .. um spaeter bei Bedarf die Knoten geordnet ausgeben zu koennen (debug)
+			kindKnoten.setName(token[vergleichsTokenIndex].intern());
+			
+			// Kind dem Elternknoten anfuegen
+			rootnode.getKinder().put(token[vergleichsTokenIndex], kindKnoten);
+			
+			// Ggf. Graphen aktualisieren
+			if (graph != null) {
+				// Kante
+				Kante neueKante = new Kante(token[vergleichsTokenIndex].intern());
+				graph.addEdge(neueKante, rootnode, kindKnoten,
+						EdgeType.DIRECTED);
+			}
 
-	    if (umgekehrt) {
-		knotenerstellt += this.baumBuilder(
-			Arrays.copyOfRange(token, 0, token.length - 1),
-			neuerKnoten, graph, umgekehrt);
-	    } else {
-		knotenerstellt += this.baumBuilder(
-			Arrays.copyOfRange(token, 1, token.length),
-			neuerKnoten, graph, umgekehrt);
-	    }
+		} else {
+			// passender Knoten vorhanden
+			kindKnoten = rootnode.getKinder().get(token[0]);
+			
+			// "Beruehrung" des existierenden Knotens mitzaehlen
+			kindKnoten.setZaehler(kindKnoten.getZaehler() + 1);
+		}
 
-	} else {
-	    // passender Knoten vorhanden - Zaehler inkrementieren und Rekursiv
-	    // aufrufen
-	    Knoten zielKnoten = rootnode.getKinder().get(token[0]);
-	    zielKnoten.setZaehler(zielKnoten.getZaehler() + 1);
+		// Pruefen, ob der Baum "umgekehrt" erstellt werden soll
+		if (umgekehrt) {
+			// Rekursiver Aufruf mit Token 0 bis n-1
+			knotenerstellt += this.baumBuilder(
+					Arrays.copyOfRange(token, 0, token.length - 1),
+					kindKnoten, graph, umgekehrt);
+		} else {
+			// Rekursiver Aufruf mit Token 1 bis n
+			knotenerstellt += this.baumBuilder(
+					Arrays.copyOfRange(token, 1, token.length),
+					kindKnoten, graph, umgekehrt);
+		}
 
-	    if (umgekehrt) {
-		knotenerstellt += this.baumBuilder(
-			Arrays.copyOfRange(token, 0, token.length - 1),
-			zielKnoten, graph, umgekehrt);
-	    } else {
-		knotenerstellt += this.baumBuilder(
-			Arrays.copyOfRange(token, 1, token.length), zielKnoten,
-			graph, umgekehrt);
-	    }
-	}
-
-	return knotenerstellt;
+		// Anzahl der neu erstellten Knoten zurueckgeben
+		return knotenerstellt;
 
     }
 
